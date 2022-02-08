5908fccbc17a8c34f06ef308ee5244a346352633
Marcio Vinicius de Souza da Rocha <marcio.souza@mps.com.br>
2022-02-08 16:55:55 -0300

    iniciado desenvolvimento de identificação de flags

--- montar.sh
+++ montar.sh
@@ -1,10 +1,35 @@
 #! /bin/bash
 
-source ./utils.sh
-source config.cgf
+# verifica se o arquivo de utilidades existe antes de importar
+if [ -f utils.sh ] then
+    source utils.sh
+else
+    ./mensagens/pt-br/utils_nao_encontrado
+    exit 1
+fi
+
+# verifica se o arquivo de configurções existe antes de importar
+if [ -f config.cfg ] then
+    source config.cgf
+else
+    ./mensagens/pt-br/config_nao_encontrado
+    exit 1
+fi
 
 check_root
 
-helpFunction(){
+function helpFunction(){
     printf 
 }
+
+# Argumentos:
+# -d especifica o Device a ser montado 
+
+while getopts d:A: flag
+do
+    case "${flag}" in
+        *) 
+            ./$msgFolder/opcao_invalida
+            exit 1
+    esac
+done
