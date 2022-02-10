9d8cc65a1484df37c15200a9641c98b61954f0ed
Marcio Rocha <dev.marcio.rocha@gmail.com>
2022-02-09 22:46:09 -0300

    iniciado desenvolvimento de funções para trabalhar com manipulação dos Blockdevices

--- utils.sh
+++ utils.sh
@@ -9,14 +9,15 @@ else
     exit 1 
 fi
 
+# TODO -c CheckRoot -t ARRUMAR
 # função para verificar se o comando está sendo executado por
 # usuário root
-function check_root(){
-    if [ [ $EUID -ne 0 ] && ROOT ]; then
-        echo "This script must be run as root."
-        exit 1
-    fi
-}
+#function check_root(){
+#    if [ [ $EUID -ne 0 ] && ROOT ]; then
+#        echo "This script must be run as root."
+#        exit 1
+#    fi
+#}
 
 # funcao para verificar se o diretório existe
 # retorna 0 caso o diretorio exista
@@ -32,8 +33,29 @@ function check_file(){
     [ -f "$1" ] && return 0 || return 1
 }
 
+# TODO -c "Extrair dados de objeto JSON" -t TERMINAR
+# funcao para extrair as variaveis passadas por parametro 
+# do objeto json
+function extrair_json(){
+    tags=()
+    for tag in "$@"; do
+        tags+="$tag"
+    done
+}
+
+# TODO -c "Blockdevices como JSON" -t TESTAR
+# retorna uma string com objetos JSON
+# referentes aos blockdevices
+function blockdevices_json(){
+    return "$(lsblk -J)"
+}
+
+# TODO -c "Listar Blockdevices" -t TERMINAR
 # funcao para listar todos os devices aliases disponíveis 
 # indicando quais ja estao montados
-function list_devices(){
-
+function listar_blockdevices(){
+    jsonList=$(jq -r '.blockdevices' <<< devices_as_json)
+    for data in $jsonList; do
+        echo "$data"
+    done
 }
