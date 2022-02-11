acd67c56d8d934336acdf8b5b27ff5ef4e7f3e7b
Marcio Rocha <dev.marcio.rocha@gmail.com>
2022-02-10 22:53:44 -0300

    adicionadas funções de trim ltrim e rtrim; arrumada função de listar blockdevices

--- utils.sh
+++ utils.sh
@@ -13,7 +13,7 @@ fi
 # TODO -c CheckRoot -t ARRUMAR
 # função para verificar se o comando está sendo executado por
 # usuário root
-#function check_root(){
+# check_root(){
 #    if [ [ $EUID -ne 0 ] && ROOT ]; then
 #        echo "This script must be run as root."
 #        exit 1
@@ -23,40 +23,92 @@ fi
 # funcao para verificar se o diretório existe
 # retorna 0 caso o diretorio exista
 # retorna 1 caso o diretorio não exista
-function check_dir(){
+check_dir(){
     [ -d "$1" ] && return 0 || return 1
 }
 
 # funcao para verificar se o arquivo em questão existe
 # retorna 0 caso o arquivo exista
 # retorna 1 caso o arquivo não exista
-function check_file(){
+check_file(){
     [ -f "$1" ] && return 0 || return 1
 }
 
 # TODO -c "Extrair dados de objeto JSON" -t TERMINAR
 # funcao para extrair as variaveis passadas por parametro 
 # do objeto json
-function extrair_json(){
+extrair_json(){
     tags=()
     for tag in "$@"; do
         tags+=("$tag")
     done
 }
 
-# TODO -c "Blockdevices como JSON" -t TESTAR
-# retorna uma string com objetos JSON
-# referentes aos blockdevices
-function blockdevices_json(){
-    return "$(lsblk -J)"
-}
-
-# TODO -c "Listar Blockdevices" -t TERMINAR
 # funcao para listar todos os devices aliases disponíveis 
 # indicando quais ja estao montados
-function listar_blockdevices(){
-    jsonList=$(jq -r '.blockdevices' <<< devices_as_json)
-    for data in $jsonList; do
-        echo "$data"
+listar_blockdevices(){
+    mapfile blockdevices <<< "$(lsblk -Pno NAME,TYPE | sed -n '/disk/p' | sed 's/NAME="//' | sed 's/" TYPE="disk"//')"
+    for e in "${blockdevices[@]}" ; do
+        echo "Blocdevice: $e"
     done
+    
 }
+
+# funcao para remover todos espacos de uma string passada como parâmetro
+# funcao para remover os espaços a direita
+# exemplo:
+#    trim "...   teste teste teste   ..."
+#    retorno "...testetesteteste..."
+trim(){
+    if [ -n "$1" ] ; then 
+        var="$1"
+    else 
+        "$msgFolder"/parametro_nao_informado
+        exit 1
+    fi
+    echo "$(tr -d '[:space:]' <<< "$var")"
+}
+
+# funcao para remover os espaços a direita
+# exemplo:
+#    rtrim "...   teste teste teste   ..."
+#    retorno "...   teste teste teste..."
+rtrim(){
+    if [ -n "$1" ] ; then 
+        var="$1"
+    else 
+        "$msgFolder"/parametro_nao_informado
+        exit 1
+    fi
+    echo "$(echo "$var" | sed -e 's/^[[:space:]]*//')"
+}
+
+# funcao para remover os espaços a esquerda
+# funcao para remover os espaços a direita
+# exemplo:
+#    trim "...   teste teste teste   ..."
+#    retorno "...teste teste teste  "
+ltrim(){
+    if [ -n "$1" ] ; then 
+        var="$1"
+    else 
+        $msgFolder/parametro_nao_informado
+        exit 1
+    fi
+    echo "$(sed -e 's/[[:space:]]*$//' <<< "$var")"
+}
+
+
+
+#teste(){
+#    jsonList=$(jq -r '.blockdevices'[] <<< $(lsblk -J))
+#    echo $jsonList
+#    for data in $jsonList; do
+#        echo $data
+#    done
+#}
+
+teste(){
+    echo ""
+}
+
